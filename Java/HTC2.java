f52841468669dee2ede195a00e667bd997ff3773
shiki3iki <eschigna@gmail.com>
2019-07-24 21:50:44 -0600

    wq

---
+++ Java/HTC2.java
@@ -0,0 +1,11 @@
+// Ask for 2 numbers and print the result of the basic operations
+
+public class HTC2
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Just a test");
+  }
+}
+  
+
